2262251c647320242eaa6092881cae0449e2a847
Kevin <currysing@gmail.com>
2019-01-16 16:26:55 +0800

    Initial Commit

---
+++ src/main/java/com/example/jpah2m2m/JpaH2M2mApplication.java
@@ -0,0 +1,14 @@
+package com.example.jpah2m2m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaH2M2mApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaH2M2mApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/jpah2m2m/JpaH2M2mApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.jpah2m2m;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JpaH2M2mApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
